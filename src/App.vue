261d703774f5c5c60cccd5382267f750fbdb7895
Lin Masahiro <k80092@gmail.com>
2020-06-15 11:41:30 +0900

    try component-api

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,41 @@
 <template>
-  <img src="./logo.png">
-  <h1>Hello Vue 3!</h1>
-  <button @click="inc">Clicked {{ count }} times.</button>
+  <div>
+    <img src="./logo.png" />
+    <h1>Hello Vue 3!</h1>
+    <button @click="inc">Clicked {{ state.count }} times.</button>
+    <p>count: {{ state.count }}</p>
+    <p>double: {{ state.double }}</p>
+    <p>count1: {{ count1 }}</p>
+    <p>count2: {{ count2 }}</p>
+  </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, reactive, computed } from "vue";
 
 export default {
   setup() {
-    const count = ref(0)
+    const state = reactive({
+      count: 0,
+      double: computed(() => state.count * 2)
+    });
+
+    let count1 = ref(0);
+    let count2 = 0;
     const inc = () => {
-      count.value++
-    }
+      count1.value++;
+      count2++
+      state.count++
+    };
 
     return {
-      count,
+      state,
+      count1,
+      count2,
       inc
-    }
+    };
   }
-}
+};
 </script>
 
 <style scoped>
